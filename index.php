0923d081b4e263f96a258f421cffe7d0da19cbc9
wszy5 <wikoszyman@wp.pl>
2020-02-01 15:57:50 +0100

    legendary first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+$data = array_fill(0,3,null); //wypelnia tablice od indexu 0 do 3 null'ami
+
+$argv = array_replace($data,$argv); //wstawia elementy z data do argv tam gdzie w argv ich nie ma
+
+list($name,$comm,$cont) = $argv; //proste przypisanie zmiennych w kolejnosci
+
+echo PHP_EOL;
+
+switch($comm)
+{
+    case 'add':
+        echo "Add TODO";
+        break;
+    case 'remove':
+        echo "Remove TODO";
+        break;
+    case null:
+        echo "List";
+        break;
+    default:
+        echo "Sorry,invalid command!";
+        break;
+}
+echo PHP_EOL;
+
+ ?>
